5337497aaa56b4a52d2052aab59ef294af7f7a51
Karol C. Gancarz <karol.sinx@gmail.com>
2022-06-22 17:12:41 +0100

    BAU: Improve logging
    Better logging for the beautify branch name, delete stacks and check stacks exist actions

--- sam/delete-stacks/delete-stacks.sh
+++ sam/delete-stacks/delete-stacks.sh
@@ -6,6 +6,8 @@ region=${AWS_REGION}
 delete_only_failed=false
 [[ ${ONLY_FAILED} == true ]] && delete_only_failed=true
 
+deleted_stacks=()
+failed_stacks=()
 for stack in "${stacks[@]}"; do
   if $delete_only_failed; then
     stack_info=$(aws cloudformation describe-stacks --stack-name "$stack")
@@ -17,5 +19,25 @@ for stack in "${stacks[@]}"; do
     fi
   fi
 
-  sam delete --no-prompts --region "$region" --stack-name "$stack"
+  sam delete --no-prompts --region "$region" --stack-name "$stack" && deleted_stacks+=("$stack") || failed_stacks+=("$stack")
 done
+
+if [[ ${#deleted_stacks[@]} -eq 1 ]]; then
+  echo "Deleted stack \`${deleted_stacks[*]}\`" >> "$GITHUB_STEP_SUMMARY"
+elif [[ ${#deleted_stacks[@]} -gt 1 ]]; then
+  echo "Deleted stacks:" >> "$GITHUB_STEP_SUMMARY"
+  for stack in "${deleted_stacks[@]}"; do
+    echo "  - $stack" >> "$GITHUB_STEP_SUMMARY"
+  done
+fi
+
+if [[ ${#failed_stacks[@]} -eq 1 ]]; then
+  echo "Failed to delete stack \`${failed_stacks[*]}\`" >> "$GITHUB_STEP_SUMMARY"
+elif [[ ${#failed_stacks[@]} -gt 1 ]]; then
+  echo "Failed to delete stacks:" >> "$GITHUB_STEP_SUMMARY"
+  for stack in "${failed_stacks[@]}"; do
+    echo "  - $stack" >> "$GITHUB_STEP_SUMMARY"
+  done
+fi
+
+cat "$GITHUB_STEP_SUMMARY"
