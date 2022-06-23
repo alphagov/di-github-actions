ad78c9dd4598dcc2518f50675d60369249890c72
Karol C. Gancarz <karol.sinx@gmail.com>
2022-06-23 12:22:15 +0100

    BAU: Check for missing file when reporting results to the job summary
    Check if the input file is missing and optionally exit with an error

--- report-step-result/append-to-step-summary.sh
+++ report-step-result/append-to-step-summary.sh
@@ -8,6 +8,14 @@ output_file=${OUT_FILE:-$GITHUB_STEP_SUMMARY}
 use_code_block=false
 [[ ${CODE_BLOCK} == true ]] && use_code_block=true
 
+fail_if_missing=false
+[[ ${FAIL_IF_FILE_MISSING} == true ]] && fail_if_missing=true
+
+if ! [[ -f $file ]]; then
+  echo "::warning title=Report missing::File $file has not been found"
+  $fail_if_missing && exit 1 || exit 0
+fi
+
 {
   [[ $message ]] && echo "**$message**"
   $use_code_block && echo '```'"$language"
