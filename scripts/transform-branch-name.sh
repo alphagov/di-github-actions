c8be889fa74b47d42943e896c0e32350e18a1038
Karol C. Gancarz <karol.sinx@gmail.com>
2022-11-07 15:13:16 +0000

    BAU: Split the beautify-branch-name action
    Move the logic from the beautify branch name action ino a script.
    Use the script instead of the action in the repo to reduce internal dependencies and simplify the workflows.

---
+++ scripts/transform-branch-name.sh
@@ -0,0 +1,21 @@
+set -eu
+
+: "${PREFIX:=}"                  # '${prefix}-' will be prepended to the returned string; included in the length limit
+: "${BRANCH_NAME:=}"             # Override the branch name to transform
+: "${LENGTH_LIMIT:=200}"         # Maximum length of the returned string
+: "${REPLACE_UNDERSCORES:=true}" # Whether to replace all underscores with hyphens
+: "${DOWNCASE:=true}"            # Whether to downcase all letters in the branch name
+
+branch_name=${PREFIX:+$PREFIX-}${BRANCH_NAME:-${GITHUB_HEAD_REF:-$GITHUB_REF_NAME}}
+
+if [[ $LENGTH_LIMIT -lt 1 ]]; then
+  echo "Invalid length limit: $LENGTH_LIMIT - must be greater than 0"
+  exit 1
+fi
+
+branch_name=$(echo "$branch_name" | tr "." "_" | tr "/" "_")
+$DOWNCASE && branch_name=$(echo "$branch_name" | tr "[:upper:]" "[:lower:]")
+$REPLACE_UNDERSCORES && branch_name=$(echo "$branch_name" | tr "_" "-")
+branch_name=$(echo "$branch_name" | cut -c1-"$LENGTH_LIMIT")
+
+echo "$branch_name"
