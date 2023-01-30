bc468fd12bcb1ee94d155b2563f9c20d1e7b8403
Karol C. Gancarz <karol.sinx@gmail.com>
2023-01-30 00:39:19 +0000

    SSE-2463: Add an option to the parse parameters action to print params in a long format
    Allow printing params in the long format "key=<key>,value='<value>'" which is useful for working with the AWS CLI when setting tags for resources.

--- scripts/parse-parameters.sh
+++ scripts/parse-parameters.sh
@@ -2,9 +2,11 @@ set -eu
 
 : "${PARAMETERS}"               # The parameters to parse
 : "${ASSOCIATIVE_ARRAY:=false}" # Whether to encode output as a string representing an associative array
+: "${LONG_FORMAT:=false}"       # Whether to encode the parameters in the form of "key=key,value='value'" strings
 
 raw_parameters=$(echo -n "${PARAMETERS}")
 associative=${ASSOCIATIVE_ARRAY}
+long=${LONG_FORMAT}
 
 num_lines=$(wc -l <<< "$raw_parameters")
 if [[ $num_lines -le 1 ]]; then
@@ -16,8 +18,14 @@ fi
 parsed_parameters=()
 for kvp in "${key_value_pairs[@]}"; do
   IFS="=" read -r name value < <(xargs <<< "$kvp")
-  name=$(xargs <<< "$name") && value=$(xargs <<< "$value")
-  $associative && element="[$name]='$value'" || element="$name='$value'"
+  name=$(xargs <<< "$name") && value="'$(xargs <<< "$value")'"
+
+  if $associative; then
+    element="[$name]=$value"
+  else
+    $long && element="key=$name,value=$value" || element="$name=$value"
+  fi
+
   parsed_parameters+=("$element")
 done
 
