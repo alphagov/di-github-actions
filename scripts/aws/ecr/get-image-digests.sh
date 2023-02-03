a1863704e189b343e9a9ec1d8817160bc29d688b
Karol C. Gancarz <karol.sinx@gmail.com>
2023-02-03 14:34:01 +0000

    SSE-2463: Use xargs to remove empty spaces and newlines from parameters and arguments
    Process tags in the build-docker-image action before using them
    Move the check-stacks-exist.sh script to the correct directory (from /aws/ecr to /aws/sam)
    
    Remove unnecessary usage of the ECR login action in the register-task-definition action. This action doesn't use the local docker client, so it doesn't need to authenticate to ECR.

--- scripts/aws/ecr/get-image-digests.sh
+++ scripts/aws/ecr/get-image-digests.sh
@@ -1,13 +1,15 @@
+# Get digests of ECR images associated with a list of tags
+# Return an associative array of tag=digest pairs
 set -eu
 
 : "${REPOSITORY}" # ECR repository name
 : "${IMAGE_TAGS}" # Tags associated with the targeted images, delimited by spaces or newlines
 
-read -ra tags <<< "$(tr '\n' ' ' <<< "$IMAGE_TAGS")"
+read -ra tags < <(xargs <<< "$IMAGE_TAGS")
 tags=("${tags[@]/#/\'}") && tags=("${tags[@]/%/\'}")
-tag_list=$(IFS=","; echo "[${tags[*]}]")
+tag_list=$(IFS="," && echo "${tags[*]}")
 
 aws ecr list-images \
   --repository-name "$REPOSITORY" \
-  --query "imageIds[?contains($tag_list, imageTag)].[imageDigest]" \
-  --output text | sort -u | tr '\n' ' '
+  --query "imageIds[?contains([$tag_list], imageTag)].[imageDigest]" \
+  --output text | sort -u | xargs
