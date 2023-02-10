57518aaab7352ff25b417b61016e3063cbdd19d4
Karol C. Gancarz <karol.sinx@gmail.com>
2023-02-10 14:00:57 +0000

    SSE-2496: Improvements to deleting SAM stacks
    Add functionality to the `sam/delete-stacks` action:
      - Add option to log into AWS if the role ARN is provided
      - Log stack names that were in a good state and ignored for deletion

--- sam/delete-stacks/delete-stacks.sh
+++ sam/delete-stacks/delete-stacks.sh
@@ -1,28 +1,30 @@
 set -eu
 
-region=${AWS_REGION}
 delete_only_failed=${ONLY_FAILED}
-
-deleted_stacks=()
-failed_stacks=()
+failed=()
 
 read -ra stacks < <(xargs <<< "${STACK_NAMES}")
 
 for stack in "${stacks[@]}"; do
   if $delete_only_failed; then
-    stack_info=$(aws cloudformation describe-stacks --stack-name "$stack")
-    stack_state=$(jq -r '.Stacks[].StackStatus' <<< "$stack_info")
+    stack_state=$(aws cloudformation describe-stacks \
+      --stack-name "$stack" \
+      --query "Stacks[].StackStatus" \
+      --output text)
 
-    if ! [[ $stack_state =~ FAILED ]]; then
-      echo "Stack '$stack' is in a good state: '$stack_state' - not deleting"
+    if ! [[ $stack_state =~ _FAILED$ ]]; then
+      ignored+=("$stack")
       continue
     fi
   fi
 
-  sam delete --no-prompts --region "$region" --stack-name "$stack" && deleted_stacks+=("$stack") || failed_stacks+=("$stack")
+  sam delete --no-prompts --region "$AWS_REGION" --stack-name "$stack" && deleted+=("$stack") || failed+=("$stack")
 done
 
-echo "deleted-stacks=${deleted_stacks[*]}" >> "$GITHUB_OUTPUT"
-echo "failed-stacks=${failed_stacks[*]}" >> "$GITHUB_OUTPUT"
+{
+  echo "deleted-stacks=${deleted[*]}"
+  echo "failed-stacks=${failed[*]}"
+  echo "ignored-stacks=${ignored[*]}"
+} >> "$GITHUB_OUTPUT"
 
-[[ ${#failed_stacks[@]} -eq 0 ]] || exit 1
+[[ ${#failed[@]} -eq 0 ]] || exit 1
